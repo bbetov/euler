715e38520e2f271fd6eac308a084879760ec152c
Branimir Betov <b_betov@yahoo.com>
2016-06-29 17:16:31 +0300

    Added problem8

---
+++ problem8/problem8.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func readInput(path string) ([]int, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var nums []int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		for _, c := range scanner.Text() {
+			if i, err := strconv.ParseInt(string(c), 10, 8); err == nil {
+				nums = append(nums, int(i))
+			}
+		}
+	}
+	return nums, scanner.Err()
+}
+
+func main() {
+	nums, _ := readInput("input.txt")
+	println(len(nums))
+	var s int
+	for h := 12; h < len(nums); h++ {
+		cs := 1
+		for _, i := range nums[h-12 : h+1] {
+			cs *= i
+		}
+		if s < cs {
+			s = cs
+		}
+	}
+	println(s)
+}
