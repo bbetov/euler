ea903389f17d018a92e0dfb039e5865ce158d6b9
Branimir Betov <b_betov@yahoo.com>
2016-07-03 20:02:22 +0300

    Added 2 solutions for problem 12

---
+++ problem_012/problem_012.go
@@ -0,0 +1,25 @@
+package main
+
+import "github.com/bbetov/euler/shared"
+
+func main() {
+	num := uint64(1)
+	for tn := uint64(1); ; tn += num {
+		divcnt := 2
+		sq := shared.SqrtUInt64(tn)
+		for d := uint64(2); d < sq; d++ {
+			if tn%d == 0 {
+				divcnt++
+				if tn/d > sq {
+					divcnt++
+				}
+			}
+		}
+		if divcnt > 500 {
+			println(tn)
+			println(divcnt)
+			break
+		}
+		num++
+	}
+}
---
+++ problem_012a/problem_012a.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/bbetov/euler/shared"
+)
+
+// Use prime factorization and determine all divisors from it
+
+func main() {
+	num := uint64(1)
+	primes := shared.GetPrimesInt64(100000)
+	for tn := uint64(1); ; tn += num {
+		divcnt := 2
+		sq := shared.SqrtUInt64(tn)
+		if sq > primes[len(primes)-1] {
+			primes = shared.GetPrimesInt64(sq * 10)
+		}
+		pd := make(map[uint64]int)
+		for _, pr := range primes {
+			if pr >= tn {
+				break
+			}
+			for dv := tn; dv%pr == 0; dv /= pr {
+				pd[pr]++
+			}
+		}
+		divcnt = 1
+		for _, v := range pd {
+			divcnt *= v + 1
+		}
+		if divcnt == 1 {
+			divcnt++
+		}
+
+		fmt.Printf("%d ==> %d \n", tn, divcnt)
+		if divcnt > 500 {
+			println(tn)
+			println(divcnt)
+			break
+		}
+		num++
+	}
+}
