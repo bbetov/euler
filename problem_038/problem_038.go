274bd233fc28095735c2415fedf0c7447c0f154f
bbetov <b_betov@yahoo.com>
2016-08-12 19:50:29 -0700

    Added problem 38

---
+++ problem_038/problem_038.go
@@ -0,0 +1,138 @@
+package main
+
+import "fmt"
+
+func isMobile(perm []int, dir []bool, index int) bool {
+	if dir[index] {
+		if index == len(dir)-1 || perm[index] < perm[index+1] {
+			return false
+		}
+	} else {
+		if index == 0 || perm[index] < perm[index-1] {
+			return false
+		}
+	}
+	return true
+}
+
+func nextPerm(perm []int, dir []bool) (p []int, d []bool) {
+	// Find largest mobile number
+	mobilePos := -1
+	var prevMobile int
+
+	//find the largest mobile integer k
+	for i := 0; i < len(perm); i++ {
+		if isMobile(perm, dir, i) {
+			if prevMobile < perm[i] {
+				mobilePos = i
+				prevMobile = perm[i]
+			}
+		}
+	}
+	if mobilePos < 0 {
+		return nil, nil
+	}
+
+	//swap k and the adjacent integer it is looking at
+	if dir[mobilePos] {
+		perm[mobilePos], perm[mobilePos+1] = perm[mobilePos+1], perm[mobilePos]
+		dir[mobilePos], dir[mobilePos+1] = dir[mobilePos+1], dir[mobilePos]
+	} else {
+		perm[mobilePos], perm[mobilePos-1] = perm[mobilePos-1], perm[mobilePos]
+		dir[mobilePos], dir[mobilePos-1] = dir[mobilePos-1], dir[mobilePos]
+	}
+	//reverse the direction of all integers larger than k
+	for i := 0; i < len(perm); i++ {
+		if perm[i] > prevMobile {
+			dir[i] = !dir[i]
+		}
+	}
+	return perm, dir
+}
+
+func getNum(s []int) (n int) {
+	mul := 1
+	for i := len(s) - 1; i >= 0; i-- {
+		n += s[i] * mul
+		mul *= 10
+	}
+	return n
+}
+
+func fillBits(n int, fl uint16) (uint16, bool) {
+	for n > 0 {
+		r := uint(n % 10)
+		bit := uint16(1 << r)
+		if r == 0 || fl&bit > 0 { // already found or zero
+			return fl, false
+		}
+		fl |= bit
+		n /= 10
+	}
+	return fl, true
+}
+
+func processPermutation(perm []int) (mul int, ok bool) {
+	for l := 1; l < len(perm)-1; l++ {
+		n := getNum(perm[:l])
+		if n == 1 {
+			continue
+		}
+		ci := 0
+		var br bool
+		for i := 1; i < 9; i++ {
+			if ci == 9 {
+				break
+			}
+			p := n * i
+			tmp := p
+			var check []int
+			for tmp > 0 {
+				r := tmp % 10
+				tmp /= 10
+				check = append(check, r)
+			}
+			for q := len(check) - 1; q >= 0; q-- {
+				if ci >= len(perm) || perm[ci] != check[q] {
+					br = true
+					break
+				}
+				ci++
+			}
+			if br {
+				break
+			}
+		}
+		if !br {
+			return n, true
+		}
+	}
+	return 0, false
+}
+
+func main() {
+	perm := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	// true to the right, false to the left
+	dir := []bool{false, false, false, false, false, false, false, false, false}
+
+	/* tests
+	pp, oo := processPermutation([]int{1, 9, 2, 3, 8, 4, 5, 7, 6})
+	fmt.Printf("%v, %v\n", pp, oo)
+	pp, oo = processPermutation([]int{9, 1, 8, 2, 7, 3, 6, 4, 5})
+	fmt.Printf("%v, %v\n", pp, oo)
+	*/
+	maxNum, seed := 0, 0
+	for ; perm != nil; perm, dir = nextPerm(perm, dir) {
+		//fmt.Printf("%v\n", perm)
+		if n, ok := processPermutation(perm); ok {
+			p := getNum(perm)
+			fmt.Printf("Found: %v, Seed: %v\n", p, n)
+			if maxNum < p {
+				maxNum = p
+				seed = n
+			}
+		}
+	}
+	fmt.Printf("Max Found: %v, Seed: %v\n", maxNum, seed)
+
+}
