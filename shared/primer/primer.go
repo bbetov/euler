5050b44aa711e25854c6866b602d415c21e31bd2
bbetov <b_betov@yahoo.com>
2016-09-24 16:57:57 -0700

    Optimizations for generating the primes

--- shared/primer/primer.go
+++ shared/primer/primer.go
@@ -22,56 +22,77 @@ const maxUint64 = ^uint64(0)
 func (p *primer) NextPrime() (n uint64, err error) {
 	p.primeIndex++
 	if p.primeIndex >= len(p.primes) {
-		if err := p.fillNext(); err != nil {
-			return 0, err
+		for {
+			n, err := p.fillNext()
+			if err != nil {
+				return 0, err
+			}
+			if n > 0 {
+				break
+			}
 		}
 	}
 	return p.primes[p.primeIndex], nil
 }
 
-func (p *primer) fillNext() error {
+func (p *primer) fillNext() (int, error) {
+	var cnt int
 	b := bitset.New(p.segSize+1, true)
-	low := uint64(p.segIndex)*uint64(p.segSize) + 1
-	if maxUint64-uint64(p.segSize) < low {
-		return errors.New("At the end of the uint64 range")
+	low := uint64(p.segIndex)*uint64(p.segSize)*2 + 3
+	if maxUint64-uint64(p.segSize)*2 < low {
+		return 0, errors.New("At the end of the uint64 range")
 	}
 	p.segIndex++
-	high := uint64(p.segIndex) * uint64(p.segSize)
-	for i := uint32(0); i < uint32(len(p.primes)); i++ {
+	high := uint64(p.segIndex)*uint64(p.segSize)*2 + 3
+	for i := uint32(1); /*skip 2*/ i < uint32(len(p.primes)); i++ {
 		start := (low / p.primes[i]) * p.primes[i]
 		if start < low {
 			start += p.primes[i]
 		}
 
 		for j := start; j < high; j += p.primes[i] {
-			b.Set(uint32(j-low), false)
+			if j%2 == 0 {
+				continue
+			}
+			idx := uint32((j - low + 1) / 2)
+			b.Set(idx, false)
 		}
 	}
 
-	for i := low; i < high; i++ {
-		if b.IsSet(uint32(i - low)) {
-			p.primes = append(p.primes, i)
+	for i := uint32(0); i < p.segSize; i++ {
+		if b.IsSet(i) {
+			p.primes = append(p.primes, low+uint64(i)*2)
+			cnt++
 		}
 	}
-	return nil
+
+	return cnt, nil
 }
 
 func (p *primer) initialize(segSize uint32) {
 	p.segSize = segSize
 	p.segIndex = 1
 	p.primeIndex = -1
+	p.primes = append(p.primes, 2)
 	// Generate small primes
 	b := bitset.New(p.segSize+1, true)
-	for i := uint32(2); i*i <= p.segSize; i++ {
-		if b.IsSet(i) {
-			for j := i * i; j <= p.segSize; j += i {
-				b.Set(j, false)
+	maxNum := p.segSize*2 + 3
+	for i := uint32(3); i*i <= maxNum; i += 2 {
+		idx := (i - 3) / 2
+		if b.IsSet(idx) {
+			for j := i * i; j <= maxNum; j += i {
+				if j%2 == 0 {
+					continue
+				}
+				idx := (j - 3) / 2
+				b.Set(idx, false)
 			}
 		}
 	}
-	for i := uint32(2); i <= p.segSize; i++ {
+
+	for i := uint32(0); i < p.segSize; i++ {
 		if b.IsSet(i) {
-			p.primes = append(p.primes, uint64(i))
+			p.primes = append(p.primes, uint64(i)*2+3)
 		}
 	}
 }
